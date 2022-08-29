26e09e62e7ba373ab6cded78d9ac8468e82f526a
Otávio Corrêa <otaviolscorrea@hotmail.com>
2022-08-29 19:20:56 -0300

    Exercicio-4

--- Listas/Lista 1/Ex4.c
+++ Listas/Lista 1/Ex4.c
@@ -3,6 +3,20 @@
 
 int main()
 {
+    int nota, acumulaNota = 0, i = 1;
+    float media;
+
+    for (i; i < 5; i++)
+    {
+        printf("Digite a %d nota: ", i);
+        scanf("%d", &nota);
+
+        acumulaNota += nota;
+    }
+
+    media = (float)acumulaNota / (i - 1);
+
+    printf("A media da nota do Aluno eh: %.2f", media);
 
     return 0;
 }
