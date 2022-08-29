dc01fed49563454cace8f59b49477e0dcb7a0ae3
Otávio Corrêa <otaviolscorrea@hotmail.com>
2022-08-29 20:37:56 -0300

    Exercicio-4

--- Listas/Lista 1/Ex4.c
+++ Listas/Lista 1/Ex4.c
@@ -3,20 +3,38 @@
 
 int main()
 {
-    int nota, acumulaNota = 0, i = 1;
-    float media;
+    int nota, acumulaNota = 0, i = 1, j = 1;
+    float media, acumulaMedia, mediaTurma;
 
-    for (i; i < 5; i++)
+    for (i; i < 3; i++)
     {
-        printf("Digite a %d nota: ", i);
-        scanf("%d", &nota);
+        printf("##########   %d Aluno   ###########\n", i);
+        for (j; j < 5; j++)
+        {
+            printf(" Digite a %d nota: ", j);
+            scanf("%d", &nota);
+            acumulaNota += nota;
+        }
 
-        acumulaNota += nota;
+        media = (float)acumulaNota / (j - 1);
+        acumulaMedia += media;
+
+        if (media >= 6)
+        {
+            printf("Aprovado com a media: %.2f\n\n ", media);
+        }
+        else
+        {
+            printf("Reprovado com a media: %.2f\n\n ", media);
+        }
+
+        j = 1;
+        acumulaNota = 0;
     }
 
-    media = (float)acumulaNota / (i - 1);
+    mediaTurma = acumulaMedia / (i - 1);
 
-    printf("A media da nota do Aluno eh: %.2f", media);
+    printf("A media final da Turma eh: %.2f", mediaTurma);
 
     return 0;
 }
