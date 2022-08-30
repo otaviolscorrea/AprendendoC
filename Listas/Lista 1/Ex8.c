7131ef499752c440e30e5d637d547e954dc9c46f
Otávio Corrêa <otaviolscorrea@hotmail.com>
2022-08-30 20:55:57 -0300

    Exercicio-8

--- Listas/Lista 1/Ex8.c
+++ Listas/Lista 1/Ex8.c
@@ -3,6 +3,44 @@
 
 int main()
 {
-    
+    int opcSelecionada;
+    float saldoInicial, saldo, saque, deposito;
+    printf("Digite o saldo inicial da conta: R$ ");
+    scanf("%f", &saldoInicial);
+    system("cls");
+    saldo = saldoInicial;
+
+    printf("Digite:\n 1 - Deposito \n 2 - Saque \n 3 - Fim \n * Selecione a Opcao: ");
+    scanf("%d", &opcSelecionada);
+
+    while (opcSelecionada != 1 && opcSelecionada != 2 && opcSelecionada != 3)
+    {
+        printf("Opcao invalida, Digite novamente: ");
+        scanf("%d", &opcSelecionada);
+    }
+    while (opcSelecionada == 1 || opcSelecionada == 2 || opcSelecionada == 3)
+    {
+        if (opcSelecionada == 1)
+        {
+            printf("Qual o valor para deposito: R$ ");
+            scanf("%f", &deposito);
+            saldo += deposito;
+            printf("Novo saldo: R$ %.2f\n", saldo);
+            break;
+        }
+        else if (opcSelecionada == 2)
+        {
+            printf("Qual o valor para saque: R$ ");
+            scanf("%f", &saque);
+            saldo += -(saque);
+            printf("Novo saldo: R$ %.2f\n", saldo);
+
+            break;
+        }
+        else if (opcSelecionada == 3)
+        {
+            break;
+        }
+    }
     return 0;
 }
