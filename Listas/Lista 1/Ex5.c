d432e015071a1618a0f2985b14a6ac338bc4ca6d
Otávio Corrêa <otaviolscorrea@hotmail.com>
2022-08-30 20:10:38 -0300

    Exercicio-5

--- Listas/Lista 1/Ex5.c
+++ Listas/Lista 1/Ex5.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-     float valor, vlcdes;
-    
-    printf("Digite o valor do produto: R$ ");
-    scanf("%f", &valor);
-    vlcdes=valor-(valor*0.09);
+    int n, j;
+    float t = 1.0, s, x;
 
-    printf("\nO valor com desconto eh de: R$ %.2f\n",vlcdes);
+    printf("Digite o valor de x: ");
+    scanf("%f", &x);
+    printf("\nDigite o valor de n: ");
+    scanf("%d", &n);
     
+    s = t;
+
+    for (j = 1; j <= n; j++)
+    {
+        t = (t / j) * x;
+        s = s + t;
+    }
+
+    printf("%.2f\n", s);
+
     return 0;
 }
--- Listas/Lista 1/Ex6.c
+++ Listas/Lista 1/Ex6.c
@@ -3,19 +3,6 @@
 
 int main()
 {
-    int a, b, aux=0;
-
-    printf("Digite os valores de A e B(respectivamente)\n");
-    scanf("%d",&a);
-    scanf("%d",&b);
-
-    aux=a;
-    a=b;
-    b=aux;
-    aux=0;
-
-    printf("\nO valor de A era: %d e passou para: %d\n", b, a);
-    printf("\nO valor de B era: %d e passou para: %d\n", a, b);
 
     return 0;
 }
