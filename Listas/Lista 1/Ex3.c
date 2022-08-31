0dd729d90b24e8bb06848efca4c4e779ce185da9
Otávio Corrêa <otaviolscorrea@hotmail.com>
2022-08-31 19:43:11 -0300

    Exercicio-3

--- Listas/Lista 1/Ex3.c
+++ Listas/Lista 1/Ex3.c
@@ -3,14 +3,14 @@
 
 int main()
 {
-    int num1, num2;
+    int num, num2;
 
     do
     {
         printf("\nDigite dois numeros inteiros:\n");
-        scanf("%d", &num1);
+        scanf("%d", &num);
         scanf("%d", &num2);
-    } while (num1 != num2);
+    } while (num != num2);
     printf("Os numeros digitados foram iguais\n\n\n");
     return 0;
 }
