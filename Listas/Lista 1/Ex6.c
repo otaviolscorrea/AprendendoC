0b73e826a52b84c87638007a0e1db8fbca075ded
Otávio Corrêa <otaviolscorrea@hotmail.com>
2022-08-31 18:00:50 -0300

    Exercicio-6

--- Listas/Lista 1/Ex6.c
+++ Listas/Lista 1/Ex6.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
+    int fatorial = 1, termo;
+    int i, f, n, seno, somar = 1;
+
+    printf("Digite o valor de x (seno): ");
+    scanf("%d", &seno);
+    printf("Digite o valor de n (Ate onde vai a seguencia): ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i += 2)
+    {
+        termo = pow(x, i);
+        fatorial = 1;
+        for (f = 1; f < i; f++)
+        {
+            fatorial *= f;
+        }
+    }
+
+    termo /= fatorial;
+
+    if (somar == 1)
+    {
+        seno += termo;
+        somar = 0;
+    }
+    else
+    {
+        seno -= termo;
+        somar = 1;
+    }
+
+    printf("\n %.2f\n", seno);
 
     return 0;
 }
