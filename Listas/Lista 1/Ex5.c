4b37393534327d03b02cee3b75b6cb76877d478a
Otávio Corrêa <otaviolscorrea@hotmail.com>
2022-08-31 18:00:35 -0300

    Exercicio-5

--- Listas/Lista 1/Ex5.c
+++ Listas/Lista 1/Ex5.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    int n, i = 1;
-    float t = 1.0, s = 1.0, x;
+    int n, i = 0;
+    float  x, t = 1.0, s;
 
     printf("Digite o valor de x: ");
     scanf("%f", &x);
-    printf("\nDigite o valor de n: ");
+    printf("Digite o valor de n: ");
     scanf("%d", &n);
 
-    for (i; i <= n; i++)
+    s = t;
+
+    for (i ; i <= n; i++)
     {
         t = (t / i) * x;
         s = s + t;
     }
 
-    printf("%.2f\n", s);
+    printf("\n %.2f\n", s);
 
     return 0;
 }
