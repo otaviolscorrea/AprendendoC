26532da8c7f9d5ac6e27f9ff354aac39aa47f682
Otávio Corrêa <otaviolscorrea@hotmail.com>
2022-09-05 14:11:33 -0300

    Ex4

--- Listas/Implementação da Prova/Ex4.c
+++ Listas/Implementação da Prova/Ex4.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
+    int i, n, x;
+    float dividendo = 0, divisor = 0, resultado = 0;
 
+    printf("Digite o valor de x: ");
+    scanf("%d", &x);
+
+    printf("Digite o valor de n: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        dividendo = ((pow(x, i)) + i);
+        divisor = x + i;
+    }
+
+    resultado = 2.0 * (dividendo / divisor);
+
+    printf("O resultado eh: %.2f", resultado);
     return 0;
 }
