5196bdb0e3a198ee4eef271e77e57f3ee66307fd
Otávio Corrêa <otaviolscorrea@hotmail.com>
2022-09-05 13:56:12 -0300

    Ex2

--- Listas/Implementação da Prova/Ex2.c
+++ Listas/Implementação da Prova/Ex2.c
@@ -2,7 +2,17 @@
 #include <stdlib.h>
 
 int main()
-{
+{  
+    float pedro = 1.60, joao = 1.50;
+    int cont = 0;
+
+    do
+    {
+        pedro += 0.02;
+        joao += 0.03;
+        cont++;
+    } while (joao <= pedro);   
+    printf("Foram nescessarios %d anos para Joao ser maior que pedro!", cont);
 
     return 0;
 }
