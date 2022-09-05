4dcd580a424419883b5dbcb49e9e27f16ed9c70a
Otávio Corrêa <otaviolscorrea@hotmail.com>
2022-09-05 14:11:47 -0300

    Ex5

--- Listas/Implementação da Prova/Ex5.c
+++ Listas/Implementação da Prova/Ex5.c
@@ -3,6 +3,23 @@
 
 int main()
 {
+    int A, N = 1, den = 2, S = 3;
+
+    printf("Informe o ULTIMO numero da sua matricula: ");
+    scanf("%d", &A);
+
+    while (N <= 5)
+    {
+        S = S + 3 * A + den + 2;
+        den = den + 3;
+        N = N + 1;
+        A = A + 1;
+    }
+
+    printf("A   : %d\n", A);
+    printf("S   : %d\n", S);
+    printf("N   : %d\n", N);
+    printf("DEN : %d\n", den);
 
     return 0;
 }
