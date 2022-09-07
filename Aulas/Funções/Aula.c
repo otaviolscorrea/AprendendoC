a3e00c3ba9d4cf8a55001b7a8749ebc78c098d44
Otávio Corrêa <otaviolscorrea@hotmail.com>
2022-09-06 22:07:48 -0300

    Aula de passar parametros

--- Aulas/Funções/Aula.c
+++ Aulas/Funções/Aula.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int potencia(int base, int expoente)
+{
+    int i, resultado = 1;
+    for (i = 1; i <= expoente; i++)
+    {
+        resultado *= base;
+    }
+
+    return resultado;
+}
+
 int main()
 {
-    int x;
+    int base, expoente, resultado;
+    printf("\nDigite o numero da base: ");
+    scanf("%d", &base);
+    printf("Digite o numero do expoente: ");
+    scanf("%d", &expoente);
 
-    
+    resultado = potencia(base, expoente);
+    printf("\n\nResultado: %d\n\n", resultado);
 
     return 0;
 }
