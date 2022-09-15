8235cc807be20c5bd5e37b73859ce9158f2e4957
Otávio Corrêa <otaviolscorrea@hotmail.com>
2022-09-15 19:11:35 -0300

    Ex1

--- Listas/Lista de Estudo/Lista 5 - Vetores e Strings/Ex1.c
+++ Listas/Lista de Estudo/Lista 5 - Vetores e Strings/Ex1.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TAM 20
+
 
 int main()
 {
-    int i, numeros[20], contPar = 0, contImpar = 0;
+    int i, numeros[TAM], contPar = 0, contImpar = 0;
 
-    for (i = 0; i < 20; i++)
+    for (i = 0; i < TAM; i++)
     {
         printf("Digite o %d numero: ", i + 1);
         scanf("%d", &numeros[i]);
