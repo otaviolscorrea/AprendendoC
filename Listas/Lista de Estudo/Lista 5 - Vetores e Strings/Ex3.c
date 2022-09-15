67fee320f45fd092d5e28815916c7782564c1c05
Otávio Corrêa <otaviolscorrea@hotmail.com>
2022-09-15 19:26:11 -0300

    Ex3

--- Listas/Lista de Estudo/Lista 5 - Vetores e Strings/Ex3.c
+++ Listas/Lista de Estudo/Lista 5 - Vetores e Strings/Ex3.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define TAM 10
+
 int main()
 {
     int i, vetorX[TAM], vetorY[TAM], vetorR[TAM];
+
     for (i = 0; i < TAM; i++)
     {
         printf("Digite o %d do vetor X: ", i + 1);
