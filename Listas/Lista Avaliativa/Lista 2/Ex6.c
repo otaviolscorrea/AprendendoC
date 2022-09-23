1454e8f9a43d027c904d7be76d0278a06595109b
Otávio Corrêa <otaviolscorrea@hotmail.com>
2022-09-23 13:40:13 -0300

    Comentando

--- Listas/Lista Avaliativa/Lista 2/Ex6.c
+++ Listas/Lista Avaliativa/Lista 2/Ex6.c
@@ -3,12 +3,13 @@
 #include <string.h>
 #define TAM 100
 
+// Criando o procedimento que inverte uma string
 void strinv(char str[])
 {
     int count, i;
 
     count = strlen(str);
-    
+
     printf("\nString invertida: ");
     for (i = count - 1; i >= 0; i--)
     {
@@ -16,6 +17,7 @@ void strinv(char str[])
     }
 }
 
+// Criando a função principal que le a string a ser invertida
 int main(int argc, char const *argv[])
 {
     char str[TAM];
