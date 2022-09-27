fe376eabdbc0acb1cf77b7884946495ae3934723
Otávio Corrêa <otaviolscorrea@hotmail.com>
2022-09-27 19:36:10 -0300

    Estudando

--- Listas/Lista de Estudo/Lista 1 - Variáveis, constantes, tipos de dados e E-S/ex1.c
+++ Listas/Lista de Estudo/Lista 1 - Variáveis, constantes, tipos de dados e E-S/ex1.c
@@ -1,5 +1,14 @@
-int main(int argc, char const *argv[])
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
 {
-    /* code */
+    int num;
+
+    printf("\nDigite um numero: ");
+    scanf("%d", &num);
+
+    printf("\nO antecessor eh: %d\nE o sucessor eh: %d\n\n", num - 1, num + 1);
+
     return 0;
 }
